8099212592ce9617dd96ed7ad5682dac322ebe59
Administrator <Administrator@smart20103>
2016-12-15 16:09:21 +0900

    리스트 페이징작업완료(내가 직접만든거..매우복잡 ㅠㅠ)

--- spms/src/main/java/spms/servlets/MemberListServlet.java
+++ spms/src/main/java/spms/servlets/MemberListServlet.java
@@ -2,6 +2,7 @@ package spms.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -26,18 +27,30 @@ public class MemberListServlet extends HttpServlet {
 			HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Connection conn = null;
-		Statement stmt = null;
+		PreparedStatement stmt = null;
 		ResultSet rs = null;
-
+		final int pagePerRow = 3;	// 페이지당 몇개 row 보여줄지 정하는 변수
+		final int groupPagePer = 5;	//그룹페이지를 몇개로 할지.. 페이지 보여줄 숫자 몇개로 할지..5개씩 또는 10개씩
+		int currentPage = 1;	//현재페이지는 초기값은 1페이지
+		if(request.getParameter("currentPage") != null){	//currentpage 를 get 방식으로 받으면
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));	//currentPage에 전송받은 현재페이지값을 대입한다.
+		}	//currentPage값을 전송안받으면 즉 제일 첫화면이기때문에 초기값 그대로 1가지고감
+		int totalCount = 0;
+		
 		try {
 			ServletContext sc = this.getServletContext();
 			conn = (Connection) sc.getAttribute("conn"); 
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(
-					"SELECT MNO,MNAME,EMAIL,CRE_DATE" + 
-					" FROM MEMBERS" +
-					" ORDER BY MNO ASC");
+			stmt = conn.prepareStatement("select count(mno) from members");
+			rs = stmt.executeQuery();
+			if(rs.next()){
+				totalCount = rs.getInt(1);
+			}
 			
+			//rs = stmt.executeQuery("SELECT MNO,MNAME,EMAIL,CRE_DATE FROM MEMBERS ORDER BY MNO ASC limit ?,?");
+			stmt = conn.prepareStatement("SELECT MNO,MNAME,EMAIL,CRE_DATE FROM MEMBERS ORDER BY MNO ASC limit ?,?");
+			stmt.setInt(1, (currentPage-1)*pagePerRow);
+			stmt.setInt(2, pagePerRow);
+			rs = stmt.executeQuery();
 			response.setContentType("text/html; charset=UTF-8");
 			ArrayList<Member> members = new ArrayList<Member>();
 			
@@ -53,10 +66,13 @@ public class MemberListServlet extends HttpServlet {
 			
 			// request에 회원 목록 데이터 보관한다.
 			request.setAttribute("members", members);
+			request.setAttribute("totalCount", totalCount);
+			request.setAttribute("pagePerRow", pagePerRow);
+			request.setAttribute("groupPagePer", groupPagePer);
+			request.setAttribute("currentPage", currentPage);
 			
 			// JSP로 출력을 위임한다.
-			RequestDispatcher rd = request.getRequestDispatcher(
-					"/member/MemberList.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberList.jsp");
 			rd.include(request, response);
 			
 		} catch (Exception e) {
--- spms/src/main/webapp/member/MemberList.jsp
+++ spms/src/main/webapp/member/MemberList.jsp
@@ -14,6 +14,8 @@
 	<p>
 		<a href='add'>신규 회원</a>
 	</p>
+	총회원수 : <%= request.getAttribute("totalCount") %> <br>
+	현재 페이지 : <%= request.getAttribute("currentPage") %><br>
 	<jsp:useBean id="members" scope="request" class="java.util.ArrayList" type="java.util.ArrayList<spms.vo.Member>" />
 <%
 		for (Member member : members) {
@@ -24,6 +26,50 @@
 			<%=member.getCreatedDate()%>
 			<a href='delete?no=<%=member.getNo()%>'>[삭제]</a>
 			<br>
+<%
+		}
+		
+		int totalCount = Integer.parseInt(request.getAttribute("totalCount").toString());
+		int pagePerRow = Integer.parseInt(request.getAttribute("pagePerRow").toString());
+		int groupPagePer = Integer.parseInt(request.getAttribute("groupPagePer").toString());
+		
+		int currentPage = Integer.parseInt(request.getAttribute("currentPage").toString());
+		System.out.println("currentPage:"+currentPage);
+		
+		int groupPage = (currentPage-1)/groupPagePer;	//현재페이지에 -1을(10때문에) 하고 보여줄 페지이당 개수로 나눠준다. 그럼 1~10페이지는 0을 11~20은 1이나옴
+		groupPage = groupPage*groupPagePer;	//0, 1 값을 다시 보여줄 페이지당 개수로 곱해줌 (나누고 다시 곱하는데 뒤에 일의자리 떨어내기위함임)
+		System.out.println("groupPage:"+groupPage);
+		
+		int lastPage = (totalCount/pagePerRow)+1;
+		System.out.println("lastPage:"+lastPage);
+		
+		int lastGroupPage = lastPage/groupPagePer;
+		lastGroupPage *= groupPagePer;
+		System.out.println("lastGroupPage:"+lastGroupPage);
+		
+		if(groupPage!=0){
+%>
+		<a href="<%=request.getContextPath()%>/member/list?currentPage=<%=groupPage-groupPagePer+1%>">이전</a>
+<%
+		}
+		if(lastGroupPage == groupPage){
+			for(int i=0; i< lastPage-lastGroupPage; i++){
+				%>
+					<a href="<%=request.getContextPath()%>/member/list?currentPage=<%=groupPage+i+1%>"><%= groupPage + i+1 %></a>
+				<%
+			}
+		}else{
+		
+			for(int i=0; i< groupPagePer; i++){
+%>
+				<a href="<%=request.getContextPath()%>/member/list?currentPage=<%=groupPage+i+1%>"><%= groupPage + i+1 %></a>
+<%
+			}
+		}
+		if(groupPage!=lastGroupPage){
+%>
+			<a href="<%=request.getContextPath()%>/member/list?currentPage=<%=groupPage+groupPagePer+1%>">다음</a>
+
 <%
 		}
 %>
